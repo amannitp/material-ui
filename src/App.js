c3b918afa74e79224adbda38b1cd6d4dd4f4778d
amannitp <amannitp272@gmail.com>
2022-08-09 18:20:43 +0530

    CheckBox component

--- src/App.js
+++ src/App.js
@@ -1,15 +1,32 @@
-import * as React from 'react';
+
+import React ,{useState} from 'react'
 import logo from './logo.svg';
 import './App.css';
 import ButtonGroup from '@mui/material/ButtonGroup';
 import Button from '@mui/material/Button';
 import DeleteIcon from '@mui/icons-material/Delete';
 import SendIcon from '@mui/icons-material/Send';
+import Checkbox from '@mui/material/Checkbox';
+
+function CheckBoxExample(){
+  const [checked,setChecked]=React.useState(true);
+  return(
+    <div>
+    <Checkbox
+    checked={checked}
+    onChange={(e)=>setChecked(e.target.checked)}
+    inputProps={{"aria-label":"primary checkbox"}}
+    />
+    </div>
+  )
+}
 
 function App() {
   return (
     <div className="App">
+      
       <header className="App-header">
+      <CheckBoxExample/>
       <ButtonGroup variant='contained' size='large' onClick={()=>alert("hello world")}>
         <Button
         startIcon={<SendIcon/>}
